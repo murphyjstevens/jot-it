2f851c866d85a6f59aac8c02674dfc823188d24b
Murphy Stevens <murphyjstevens@protonmail.com>
2022-04-10 21:51:58 -0500

    Set up baseline for app

---
+++ src/components/NoteItem.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import MarkdownIt from 'markdown-it'
+
+const markdownIt = new MarkdownIt()
+
+const markdown = ref('')
+
+const markdownHtml = computed(() => markdownIt.render(markdown.value))
+</script>
+
+<template>
+  <div class="flex-row justify-content-center mt-1">
+    <div class="markdown-wrapper flex-row justify-content-space-between">
+      <div class="flex-column markdown-column">
+        <label for="markdown">Markdown</label>
+        <textarea
+          id="markdown"
+          v-model="markdown"
+          class="markdown-area mr-1"
+        ></textarea>
+      </div>
+      <div class="flex-column markdown-column">
+        <label for="html">Display</label>
+        <div id="html" v-html="markdownHtml" class="markdown-area"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.markdown-wrapper {
+  width: 1000px;
+}
+
+.markdown-column {
+  width: 50%;
+}
+
+.markdown-area {
+  height: 500px;
+  background-color: var(--vt-c-black-mute);
+  color: inherit;
+  border: 1px solid #444;
+  border-radius: 4px;
+  padding: 12px 20px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <h1><a href="/">Jot-It</a></h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 0 2rem;
+  background-color: var(--color-background-soft);
+}
+</style>
