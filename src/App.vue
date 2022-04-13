d7d87da733096e182de075e614327106ad8e4abb
Murphy Stevens <murphyjstevens@protonmail.com>
2022-04-12 20:13:03 -0500

    Added nice transitions and bootstrap icons

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ import NoteItem from './components/NoteItem.vue'
 @import './assets/flex.css';
 @import './assets/inputs.css';
 @import './assets/spacing.css';
+@import 'bootstrap-icons/font/bootstrap-icons.css';
 
 main {
   max-width: 1280px;
--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -23,33 +23,37 @@ function setVisible(isVisible: boolean, isMarkdown: boolean): void {
   <div class="flex-row justify-content-center mt-1">
     <div class="markdown-wrapper flex-row justify-content-space-between">
       <button
-        title="Show Markdown"
+        title="Show markdown"
         @click="setVisible(true, true)"
-        class="floating-show-markdown"
-        :class="{ hidden: isMarkdownVisible }"
+        class="floating-show floating-show-markdown"
+        :class="{ 'hiding-show hiding-show-markdown': isMarkdownVisible }"
       >
-        Show
+        <i class="bi-eye-fill"></i>
       </button>
       <button
-        title="Show Display"
+        title="Show display"
         @click="setVisible(true, false)"
-        class="floating-show-display"
-        :class="{ hidden: isDisplayVisible }"
+        class="floating-show floating-show-display"
+        :class="{ 'hiding-show hiding-show-display': isDisplayVisible }"
       >
-        Show
+        <i class="bi-eye-fill"></i>
       </button>
       <div
         class="flex-column markdown-column"
-        :class="{ hidden: !isMarkdownVisible }"
+        :class="{
+          'minimized-column': !isMarkdownVisible,
+          'maximized-column': !isDisplayVisible,
+        }"
       >
         <div class="flex-row justify-content-space-between mr-1 ml-1">
           <label for="markdown">Markdown</label>
           <button
+            title="Hide markdown"
             @click="setVisible(false, true)"
             :disabled="!isDisplayVisible"
             class="mb-1"
           >
-            Hide
+            <i class="bi-eye-slash-fill"></i>
           </button>
         </div>
         <textarea
@@ -60,20 +64,24 @@ function setVisible(isVisible: boolean, isMarkdown: boolean): void {
       </div>
       <div
         class="markdown-column-spacer"
-        :class="{ hidden: !isDisplayVisible || !isMarkdownVisible }"
+        :class="{ 'minimized-column': !isDisplayVisible || !isMarkdownVisible }"
       ></div>
       <div
         class="flex-column markdown-column"
-        :class="{ hidden: !isDisplayVisible }"
+        :class="{
+          'minimized-column': !isDisplayVisible,
+          'maximized-column': !isMarkdownVisible,
+        }"
       >
         <div class="flex-row justify-content-space-between mr-1 ml-1">
           <label for="html">Display</label>
           <button
+            title="Hide display"
             @click="setVisible(false, false)"
             :disabled="!isMarkdownVisible"
             class="mb-1"
           >
-            Hide
+            <i class="bi-eye-slash-fill"></i>
           </button>
         </div>
         <div id="html" v-html="markdownHtml" class="markdown-area"></div>
@@ -88,11 +96,33 @@ function setVisible(isVisible: boolean, isMarkdown: boolean): void {
 }
 
 .markdown-column {
-  width: 100%;
+  width: 50%;
+  transition: width 0.5s ease-out, opacity 0.5s ease-in;
+  z-index: 2;
 }
 
 .markdown-column-spacer {
   width: 1rem;
+  transition: width 0.5s ease-out;
+  z-index: 2;
+}
+
+.minimized-column {
+  overflow: hidden;
+  width: 0px;
+  opacity: 0;
+  transition: width 0.5s ease-out, opacity 0.5s ease-in;
+}
+
+.minimized-column textarea {
+  text-overflow: clip;
+  overflow: hidden;
+  white-space: pre;
+}
+
+.maximized-column {
+  width: 100%;
+  transition: width 0.5s ease-out;
 }
 
 .markdown-area {
@@ -104,15 +134,34 @@ function setVisible(isVisible: boolean, isMarkdown: boolean): void {
   padding: 12px 20px;
 }
 
-.floating-show-markdown {
+.floating-show {
   position: absolute;
-  left: -3rem;
+  width: 1.5rem;
   top: 2rem;
+  transition: transform 0.5s ease-out, opacity 0.5s ease-in 0.3s;
+  z-index: 1;
+}
+
+.hiding-show {
+  opacity: 0;
+  transition: transform 0.5s ease-out, opacity 0.2s linear;
+}
+
+.floating-show-markdown {
+  left: -1.5rem;
+  border-radius: 10px 0px 0px 10px;
+}
+
+.hiding-show-markdown {
+  transform: translateX(3rem);
 }
 
 .floating-show-display {
-  position: absolute;
-  right: -3rem;
-  top: 2rem;
+  right: -1.5rem;
+  border-radius: 0px 10px 10px 0px;
+}
+
+.hiding-show-display {
+  transform: translateX(-3rem);
 }
 </style>
