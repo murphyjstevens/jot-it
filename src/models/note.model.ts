64546d20eefad6ff69fc414fe8519a195b7b8da2
Murphy Stevens <murphyjstevens@protonmail.com>
2022-04-13 21:15:23 -0500

    Added missed files

---
+++ src/models/note.model.ts
@@ -0,0 +1,6 @@
+export interface Note {
+  id: number
+  markdownText: string
+  creatorId: number
+  updatedDate: Date
+}
---
+++ src/stores/note.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia'
+
+import type { Note } from '../models/note.model'
+
+export const useNoteStore = defineStore('note', {
+  state: () => ({
+    notes: new Array<Note>(),
+  }),
+  actions: {
+    load() {
+      const json = localStorage.getItem('notes')
+      if (!json) {
+        this.notes = []
+        return
+      }
+
+      let noteList = JSON.parse(json) as Array<Note>
+      noteList = noteList.map((note) => {
+        return {
+          ...note,
+          updatedDate: note.updatedDate ? new Date(note.updatedDate) : null,
+        } as Note
+      })
+
+      this.notes = noteList
+    },
+    saveNote(updatedNote: Note) {
+      this.notes = [
+        ...this.notes.filter((n) => n.id !== updatedNote.id),
+        updatedNote,
+      ]
+    },
+  },
+})
